7ab81af107e0e5082dd81d266bfc37e1855ec717
Karan Panchamiya <karanpanchamiya4@gmail.com>
2026-02-04 16:27:10 +0530

    Fisrt set up

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+   this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 setup(
     name="mlproject",
@@ -6,5 +23,6 @@ setup(
     author='karan',
     author_email='karanpanchamiya4@gmail.com',
     packages=find_packages(),
-    install_requires=['numpy', 'pandas', 'scikit-learn', 'flask']
+    install_requires=get_requirements('requirements.txt')
 )
+ 
