7a3a482707bbbb619645f19ac3148d18ba2937fe
Karan Panchamiya <karanpanchamiya4@gmail.com>
2026-02-06 12:39:34 +0530

    remove incorrect exception.pysrc folder

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT = '-e .'
+HYPEN_E_DOT = "-e ."
+
 
 def get_requirements(file_path: str) -> List[str]:
-    '''
-   this function will return the list of requirements
-    '''
-    requirements=[]
+    """
+    this function will return the list of requirements
+    """
+    requirements = []
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n","") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
+        for line in file_obj:
+            line = line.strip()
+            if line and line != HYPEN_E_DOT:
+                requirements.append(line)
     return requirements
+
 
 setup(
     name="mlproject",
     version="0.1.0",
-    author='karan',
-    author_email='karanpanchamiya4@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+    author="karan",
+    author_email="karanpanchamiya4@gmail.com",
+    package_dir={"": "src"},                # ⭐ IMPORTANT
+    packages=find_packages(where="src"),   # ⭐ IMPORTANT
+    install_requires=get_requirements("requirements.txt"),
 )
- 
